067d2fd9c69f4bfdca90f12c5f341cd797d1dc52
olivi <olivi@172.29.117.61>
2024-07-25 19:15:49 -0400

    first

---
+++ src/main/java/equipe16/mavenproject8/Mavenproject8.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package equipe16.mavenproject8;
+
+/**
+ *
+ * @author olivi
+ */
+public class Mavenproject8 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
